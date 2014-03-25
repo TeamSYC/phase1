1f7be94ab2682a1c568acac847f62641ec9fd6d9
SaiWebApps <sairambkrishnan@gmail.com>
2014-03-24 23:21:47 -0400

    Query 2 MySQL

--- Undertow/project-15619/src/base/Home.java
+++ Undertow/project-15619/src/base/Home.java
@@ -1,18 +1,51 @@
 package base;
 
+import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
+import java.util.Deque;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
 public class Home {
-
+	final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	final String DB_URL = "jdbc:mysql://localhost:3306/tweet_db";
+	final String USER = "root";
+	final String PASSWORD = "db15319root";
+	
+	public String getSQLEntries(String key) {
+		try {
+			Class.forName(JDBC_DRIVER); //Register JDBC driver.
+			Connection driver = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+			Statement stmt = driver.createStatement();
+			ResultSet set = stmt.executeQuery("SELECT tweet_list FROM tweets WHERE user_time=\"" + key + "\"");
+			String results = "";
+			
+			while (set.next()) {
+				results += set.getString("tweet_list").replaceAll("_", "\n");
+			}
+			
+			driver.close();
+			return results;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+	public String getHBaseEntries() {
+		return null;
+	}
+	
 	public static void main(String[] args) {
 		final String info = "SYC,8635-0832-4410\n";
 		final SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		final String HOST = args[0];
+		final Home home = new Home();
 		
 		Undertow server = Undertow.builder()
 				.addHttpListener(80, HOST)
@@ -24,6 +57,13 @@ public class Home {
 
 						if (path.equals("/q1")) {
 							exchange.getResponseSender().send(info + fmt.format(new Date()));
+						}
+						else if (path.startsWith("/q2")) {
+							Map<String, Deque<String>> queryMap = exchange.getQueryParameters();
+							String userid = queryMap.get("userid").getFirst();
+							String tweet_time = queryMap.get("tweet_time").getFirst();
+							String result = home.getSQLEntries(userid + "_" + tweet_time);
+							exchange.getResponseSender().send(info + result);
 						}
 					}
 				}).build();
